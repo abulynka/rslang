bd85a2c4903aec8058f3113bc1fd1d41e6cf27ed
cheerfulperson <yagor.baradavkin@gmail.com>
2022-02-10 22:37:37 +0300

    feat: add sign up ang log in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { routeChangeAnimation } from './components/change-route-animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [routeChangeAnimation],
 })
 export class AppComponent {
   title = 'rslang';
+
+  getRouteAnimationState(outlet: RouterOutlet) {
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData['animation']
+    )
+  }
 }
