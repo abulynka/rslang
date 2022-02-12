f483c4f956c6b379482e108eb00ff52a05952073
Yulia Bondar <yuliabondar98@gmail.com>
2022-02-12 23:11:04 +0300

    feat: add results in audiocall game

--- src/app/components/audiocall-game/audiocall-game.component.ts
+++ src/app/components/audiocall-game/audiocall-game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionAudioCall, Word } from 'src/app/interfaces/interfaces';
 import { GamesStatesService } from 'src/app/services/games-states.service';
+import { UserProgressService } from 'src/app/services/user-progress.service';
 import { WordsService } from 'src/app/services/words.service';
 import { environment } from 'src/environments/environment';
 
@@ -9,11 +10,12 @@ import { environment } from 'src/environments/environment';
   templateUrl: './audiocall-game.component.html',
   styleUrls: ['./audiocall-game.component.scss']
 })
-export class AudiocallGameComponent {
+export class AudiocallGameComponent implements OnInit {
   public words: Word[] = [];
-  private groupNumber: number = 0;
-  private pageNumber: number = 0;
+  private group: string = '0';
+  private page: string = '0';
   public wordNumber: number = 0;
+  public state: string = '';
   public answeredAmount: number = 0;
   private audioObj: HTMLAudioElement = new Audio();
   public questions: Array<QuestionAudioCall> = [];
@@ -21,11 +23,23 @@ export class AudiocallGameComponent {
   public url: string = environment.apiUrl;
   public isCorrect: boolean = false;
   public totalAmount: number = this.questions.length;
+  public answerBullets = new Array(this.totalAmount).fill("");
+  public answerScore = new Array(this.totalAmount).fill("");
+  public correctCount: number = 0;
 
-  public constructor(private wordsService: WordsService, private GamesService: GamesStatesService) { }
+  public constructor(private wordsService: WordsService, private GamesService: GamesStatesService, private userProgressService: UserProgressService) { }
 
-  // ngOnInit(): void {
-  // }
+  public ngOnInit(): void {
+    if (!this.GamesService.isOpenedFormMenu) {
+      this.startGame();
+    }
+  }
+
+  public playSound(word: Word): void {
+    this.audioObj.src = `${this.url}/${word.audio}`;
+    this.audioObj.load();
+    this.audioObj.play();
+  }
 
   public getRandomWord(question: QuestionAudioCall): Array<Word> {
     const shuffle = (array: Array<Word>) => {
@@ -44,44 +58,27 @@ export class AudiocallGameComponent {
     return shuffle([question.answer, ...question.other]);
   }
 
-  public renderQuestion(): void {
-    const rulesContainer = document.querySelector('.rules-container') as HTMLElement;
-    rulesContainer.remove();
-    const questionContainer = document.querySelector('.question-container') as HTMLElement;
-    questionContainer.style.display = 'flex';
-    this.getWords();
-    const answerBtns = document.querySelectorAll(".answer-btn") as NodeListOf<HTMLElement>;
-    console.log(answerBtns);
-    answerBtns.forEach((item: HTMLElement) => {
-      item.addEventListener("click", () => this.chooseWord(item));
-    })
-  }
-
   private getWords(): void {
     this.wordsService
-      .getWords(this.groupNumber.toString(), this.pageNumber.toString())
+      .getWords(this.group, this.GamesService.page)
       .subscribe((words: Word[]) => {
         this.words = words;
+        this.totalAmount = this.words.length;
         this.questions = new Array(20).fill(0).map((_, index) => {
           return {
-            answer: this.words[this.groupNumber * this.pageNumber * 20 + index],
+            answer: this.words[index],
             other: new Array(4).fill(0).map(() => {
               return this.words[Math.floor(Math.random() * this.words.length)];
             }),
           };
         });
         this.randomWordsArr = this.getRandomWord(this.questions[this.answeredAmount]);
-        console.log(this.randomWordsArr)
+        this.playSound(this.questions[this.answeredAmount].answer);
       });
   }
 
-  public playSound(word: Word): void {
-    this.audioObj.src = `${this.url}/${word.audio}`;
-    this.audioObj.load();
-    this.audioObj.play();
-  }
-
   public chooseWord(item: HTMLElement): void {
+    let currentIndex = this.answeredAmount;
     const answerContainer = document.querySelector('.answer-container') as HTMLElement;
     const sound = document.querySelector('.sound') as HTMLElement;
     const btnNext = document.querySelector('.btn-next') as HTMLElement;
@@ -93,21 +90,85 @@ export class AudiocallGameComponent {
     const answerWord = document.querySelector(".answer-word") as HTMLElement;
     if (item.textContent !== answerWord.textContent) {
       item.style.background = "red";
+      this.answerBullets[currentIndex] = "red";
+      this.answerScore[currentIndex] = "red";
     } else {
+      this.answerBullets[currentIndex] = "green";
+      this.answerScore[currentIndex] = "green";
       this.isCorrect = true;
       item.style.background = "green";
+      this.correctCount++;
     }
-    this.answeredAmount++;
+    this.userProgressService.checkWord({answer: this.isCorrect, ...this.words[this.answeredAmount]}, 'audiocall');
+    const circles = document.querySelectorAll(".circle");
+    circles.forEach((circle, index) => {
+      const classCircle = this.answerBullets[index];
+      if (classCircle) circle.classList.add(classCircle);
+    });
+    const resultRows = document.querySelectorAll('.result-row');
+    resultRows.forEach((resultRow, index) => {
+      const classResultRow = this.answerScore[index];
+      if (classResultRow) resultRow.classList.add(classResultRow);
+    });
   };
 
+  public renderQuestion(num?: number): void {
+    if (num) {
+      this.group = String(num);
+    };
+    this.page = this.GamesService.setRandomPage();
+    this.getWords();
+    const rulesContainer = document.querySelector('.rules-container') as HTMLElement;
+    rulesContainer.style.display = 'none';
+    console.log()
+    const questionContainer = document.querySelector('.question-container') as HTMLElement;
+    questionContainer.style.display = 'flex';
+    const circles = document.querySelectorAll('.circle');
+    circles.forEach((item, index) => {
+      item.classList.add(`${this.answerBullets[index]}`);
+    });
+    const resultRow = document.querySelectorAll('.result-row');
+    resultRow.forEach((item, index) => {
+      item.classList.add(`${this.answerScore[index]}`);
+    });
+  }
+
+  public startGame(): void {
+    this.state = 'process';
+    this.group = this.GamesService.group;
+    this.page = this.GamesService.page;
+    this.getWords();
+  }
+
   public nextQuestion() {
+    this.answeredAmount += 1;
     if (this.answeredAmount < this.totalAmount) {
       this.renderQuestion();
-    } else {
+      const answerBtn = document.querySelectorAll('.answer-btn') as NodeListOf<HTMLElement>;
+      answerBtn.forEach((item: HTMLElement) => {
+        item.style.background = '#0d6efd';
+      })
       const answerContainer = document.querySelector('.answer-container') as HTMLElement;
       const sound = document.querySelector('.sound') as HTMLElement;
       answerContainer.style.display = "none";
       sound.style.display = "block";
+      const btnNext = document.querySelector('.btn-next') as HTMLElement;
+      const btnUnknown = document.querySelector('.btn-unknown') as HTMLElement;
+      btnNext.style.display = "none";
+      btnUnknown.style.display = "flex";
+    } else {
+      const resultPopup = document.querySelector('.result-popup') as HTMLElement;
+      resultPopup.style.display = 'block';
     }
   };
+
+  public closeResult() {
+    const resultPopup = document.querySelector('.result-popup') as HTMLElement;
+    resultPopup.style.display = "none";
+    const questionContainer = document.querySelector('.question-container') as HTMLElement;
+    questionContainer.style.display = "none";
+    const rulesContainer = document.querySelector('.rules-container') as HTMLElement;
+    rulesContainer.style.display = "flex";
+  }
+
 }
